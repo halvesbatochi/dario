fd5c2f76873f9ec64a42f4dcc093b8d6be74922d
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2023-09-15 23:44:51 -0300

    Created Struct Events View

---
+++ dario/dario/Controllers/Others/DREventsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DREventsViewController.swift
+//  dario
+//
+//  Created by Henrique Alves Batochi on 15/09/23.
+//
+
+import UIKit
+
+class DREventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ dario/dario/Views/Events/DREventsView.swift
@@ -0,0 +1,24 @@
+//
+//  DREventsView.swift
+//  dario
+//
+//  Created by Henrique Alves Batochi on 15/09/23.
+//
+
+import UIKit
+
+class DREventsView: UIView {
+
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBlue
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Privates Methods
+}
